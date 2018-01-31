6b0fe728da209d148c9a265cdf607c53dfd5c5a9
Mike Lin <mlinchu@ucsd.edu>
2018-01-30 16:01:07 -0800

    User Stories 1, 2 complete.

---
+++ Flix_Assignment1/MovieCell.swift
@@ -0,0 +1,31 @@
+//
+//  MovieCell.swift
+//  Flix_Assignment1
+//
+//  Created by Mike Lin on 1/30/18.
+//  Copyright © 2018 Mike Lin. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var overviewLabel: UILabel!
+    
+    @IBOutlet weak var posterImageView: UIImageView!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Podfile
@@ -0,0 +1,22 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Flix_Assignment1' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Flix_Assignment1
+     pod 'AlamofireImage', '~> 3.3'
+  
+
+  target 'Flix_Assignment1Tests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'Flix_Assignment1UITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
