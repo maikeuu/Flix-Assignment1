b1b87d4fcedf250fd53cbb3eec8807c1c61c9e4f
Mike Lin <mlinchu@ucsd.edu>
2018-02-07 16:15:29 -0800

    Week 2 Mandatory User Stories implemented

---
+++ Flix_Assignment1/Views/PosterCell.swift
@@ -0,0 +1,14 @@
+//
+//  PosterCell.swift
+//  Flix_Assignment1
+//
+//  Created by Mike Lin on 2/6/18.
+//  Copyright © 2018 Mike Lin. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterImageView: UIImageView!
+}
